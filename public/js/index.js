17f924200dae018fd7e8cc70ad159f82b57ea864
karboosx <karbo243@gmail.com>
2017-03-14 22:54:34 +0100

    link to chat on index

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 $(function () {
     var linkInput = $('#link');
+    var go_to_link = $('#go_to_link');
 
     $.get('/get-link').done(function (data) {
 
         linkInput.val(window.location.href+'chat/'+data.conversation_id);
+        go_to_link.prop('href',window.location.href+'chat/'+data.conversation_id);
         linkInput.prop('disabled', false);
     });
 
